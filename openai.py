9457a046d7ea73b4212ed2120321a4e53b6ea681
jong-0126 <baek011226@naver.com>
2026-01-27 15:32:45 +0900

    feat: openai  설정

--- openai.py
+++ openai.py
@@ -1,15 +1,15 @@
 from dotenv import load_dotenv
-from langchain_openai import ChatOpneAI
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-gpt4_mini = ChatOpneAI(
+gpt4_mini = ChatOpenAI(
     model = "gpt-4.1-mini",
     temperature = 0.7,
     max_tokens = 150
 )
 
-gpt4 = ChatOpneAI(
+gpt4 = ChatOpenAI(
     model = "gpt-4.1",
     temperature = 0.7,
     max_tokens = 300
