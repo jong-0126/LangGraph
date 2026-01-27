5d6caa5cb50fa945342ba7b346ff72c2365b1849
jong-0126 <baek011226@naver.com>
2026-01-27 17:12:49 +0900

    gemini api 호출

--- langchain_chat_test.py
+++ langchain_chat_test.py
@@ -1,8 +1,11 @@
 from langchain_core.messages import HumanMessage
-from openai_client import gpt4_mini, gpt4
+from openai_client import gemini
 
-response_mini = gpt4_mini.invoke([HumanMessage(content="Hello, how are you?")])
-print(response_mini.content)
+gemini_response = gemini.invoke("Explain the components of LangGraph.")
+print("Gemini's response:", gemini_response)
 
-reponse_full = gpt4.invoke([HumanMessage(content="Explain the concept of machine learning.")])
-print(reponse_full.content)
+# response_mini = gpt4_mini.invoke([HumanMessage(content="Hello, how are you?")])
+# print(response_mini.content)
+
+# reponse_full = gpt4.invoke([HumanMessage(content="Explain the concept of machine learning.")])
+# print(reponse_full.content)
--- openai_client.py
+++ openai_client.py
@@ -1,16 +1,22 @@
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-gpt4_mini = ChatOpenAI(
-    model = "gpt-4.1-mini",
-    temperature = 0.7,
-    max_tokens = 150
+gemini = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
 )
 
-gpt4 = ChatOpenAI(
-    model = "gpt-4.1",
-    temperature = 0.7,
-    max_tokens = 300
-)
+# gpt4_mini = ChatOpenAI(
+#     model = "gpt-4.1-mini",
+#     temperature = 0.7,
+#     max_tokens = 150
+# )
+
+# gpt4 = ChatOpenAI(
+#     model = "gpt-4.1",
+#     temperature = 0.7,
+#     max_tokens = 300
+# )
