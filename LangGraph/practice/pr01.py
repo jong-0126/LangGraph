190c5655b72f496a1b18f1e6d839c27118353f23
jong-0126 <baek011226@naver.com>
2026-01-28 15:16:42 +0900

    feat: 누적 log 추가

--- LangGraph/practice/pr01.py
+++ LangGraph/practice/pr01.py
@@ -1,18 +1,32 @@
-from typing import TypedDict
+from typing import TypedDict, Annotated
 from langgraph.graph import StateGraph, START, END
+from operator import add
 
 class State(TypedDict):
     text: str
     output: str
+    log: Annotated[list[str], add]
 
+# text output으로 복사
 def echo_node(state: State) -> dict:
-    return {"output": state["text"]}
+    return {
+        "output": state["text"],
+        "log": [f"text: {state['text']}", f"echo done"]
+    }
 
+# 대문자로 변경
 def upper_node(state:State) -> dict:
-    return {"output": state["output"].upper()}
+    return {
+        "output": state["output"].upper(),
+        "log": [f"text: {state['output']}", f"upper done"]
+    }
 
+# ! 추가
 def mark_node(state: State) -> dict:
-    return {"output": state["output"] + "!"}
+    return {
+        "output": state["output"] + "!",
+        "log": [f"text: {state['output']}", f"mark done"]
+    }
 
 builder = StateGraph(State)
 
@@ -27,7 +41,7 @@ builder.add_edge("upper", "mark")
 builder.add_edge("mark", END)
 
 graph = builder.compile()
-print(graph.invoke({"text":"hello"}))
+print(graph.invoke({"text":"hello", "log": []}))
 
 
 
